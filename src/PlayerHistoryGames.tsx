7d89d613dad05249f875e14b9d89514494c8611c
Mauri <pizarromauricio031096@gmail.com>
2024-05-25 19:59:59 -0300

    add-skeleton-and-socketIO

--- src/PlayerHistoryGames.tsx
+++ src/PlayerHistoryGames.tsx
@@ -1,7 +1,9 @@
-
 import React, { useEffect, useState } from 'react';
 import './PlayerHistoryGames.css';
 import { getUserIdFromCookies } from './utils/GetUserIdFromCookies';
+import io from 'socket.io-client';
+import Skeleton, { SkeletonTheme } from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
 
 interface Game {
   game_id: string;
@@ -9,11 +11,13 @@ interface Game {
   player_status: string;
 }
 
+const socket = io('http://localhost:3000');
 const playerId = getUserIdFromCookies();
 
 const PlayerHistoryGames: React.FC = () => {
   const [games, setGames] = useState<Game[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
     const fetchGames = async () => {
@@ -23,20 +27,43 @@ const PlayerHistoryGames: React.FC = () => {
           throw new Error('Error fetching data');
         }
         const data = await response.json();
-        const latestGames = data.results.reverse().slice(0, 5);
+        const latestGames = data.results?.reverse().slice(0, 5) || [];
         setGames(latestGames);
       } catch (err) {
-        
+        setError('Failed to fetch game history');
       } finally {
         setLoading(false);
       }
     };
 
+    socket.on('newGame', fetchGames);
     fetchGames();
+
+    return () => {
+      socket.off('newGame', fetchGames);
+    };
   }, [playerId]);
 
   if (loading) {
-    return <div>Loading...</div>;
+    return (
+      <div className="player-history">
+        <h2>Last games</h2>
+        <ul>
+          {[...Array(5)].map((_, index) => (
+            <li key={index} className="game-item">
+               <SkeletonTheme baseColor="#1c1c1c" highlightColor="#f5f5f5">
+                <div><Skeleton width={100} height={20} /></div>
+                <div><Skeleton width={50} height={20} /></div>
+              </SkeletonTheme>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+
+  if (error) {
+    return <div className="player-history"><h6>{error}</h6></div>;
   }
 
   return (
@@ -46,7 +73,7 @@ const PlayerHistoryGames: React.FC = () => {
         {games.map(game => (
           <li key={game.game_id} className="game-item">
             <div>Status: {game.status}</div>
-            <div style={{ color: game.player_status === 'winner' ? 'green' : game.player_status === 'loser' ? 'red' : game.player_status === 'playing' ? 'yellow' : 'red' }}>
+            <div style={{ color: game.player_status === 'winner' ? 'green' : game.player_status === 'looser' ? 'red' : game.player_status === 'playing' ? 'yellow' : 'white' }}>
               {game.player_status}
             </div>
           </li>
